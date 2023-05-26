661a40182e85e7eade2f8d91e09a76af57415525
Anjali-V786 <133162767+Anjali-V786@users.noreply.github.com>
2023-05-25 23:35:48 -0400

    Update CardTrick.java

--- src/CardTrick.java
+++ src/CardTrick.java
@@ -9,7 +9,22 @@
  */
 public class CardTrick {
     private static final String[]suits = {
-     "2","3","4","5","6","7","8","9","10", "Ace", "King","Queen","Jack"};   
+     "2","3","4","5","6","7","8","9","10", "Ace", "King","Queen","Jack"};  
+    private static final int HAND_SIZE= 7;
+      private String[]hand;
+      public CardTrick(){
+          hand = new String[HAND_SIZE];
+          fillHandWithRandomCards();
+      }
+
+    private void fillHandWithRandomCards() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+     Random input = new Random();
+     for(int n =0; n< HAND_SIZE;n++){
+         String inputSuit = suits[input.nextInt(suits.length)];
+         String inputValue =values[input.nextInt(values.length)];
+         hand[n]= inputValue + "of" + inputSuit;
+     }
     
     
 }
